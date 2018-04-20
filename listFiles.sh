a550b35a7b2a542c7685375cc6e27138b93f56e7
Lucas Miranda <miranda.a.lucas@gmail.com>
2018-04-20 18:01:33 -0300

    Update to remove .lua files from repository

--- listFiles.sh
+++ listFiles.sh
@@ -4,7 +4,7 @@ find=C:\\cmder\\vendor\\git-for-windows\\usr\\bin\\find.exe
 
 if [ -n "$1" ]; then
     if [ "$1" = "lua" ]; then
-        $find ./lua -path ./lua/lib -prune -o \( -name '*.lua' -o -path './lua/*' \) -print
+        $find ./lua -name "*.lnk" -prune -o \( -name "*.lua" -o -path "./lua/*" \) -print
     elif [ "$1" = "bin" ]; then
         echo ""
     fi
