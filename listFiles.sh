8388287396eb8259e3941b779f75922d8f2d5b12
Lucas Miranda <miranda.a.lucas@gmail.com>
2018-04-21 11:02:36 -0300

    Update to listFiles.sh to ignore content folder

--- listFiles.sh
+++ listFiles.sh
@@ -4,7 +4,7 @@ find=C:\\cmder\\vendor\\git-for-windows\\usr\\bin\\find.exe
 
 if [ -n "$1" ]; then
     if [ "$1" = "lua" ]; then
-        $find ./lua -name "*.lnk" -prune -o \( -name "*.lua" -o -path "./lua/*" \) -print
+        $find ./lua -name "*.lnk" -prune -o -path "./content" -prune \( -name "*.lua" -o -path "./lua/*" \) -print
     elif [ "$1" = "bin" ]; then
         echo ""
     fi
